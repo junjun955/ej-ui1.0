bdb47af1fead2ef1499f70be60d5003518f1e1f7
qpl123 <1695521701@qq.com>
2019-06-11 18:46:19 +0800

    修改端口

--- src/router.js
+++ src/router.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import OrderLinePage from './routes/OrderLinePage'
 import CustomerPage from './routes/CustomerPage'
-<<<<<<< HEAD
-=======
-import OrderPage from './routes/OrderPage'
->>>>>>> c668779ee15aed07b5681b41cd957310baeeb613
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
-        <Route path="/orderLine" exact component={OrderLinePage} />
         <Route path="/customer" exact component={CustomerPage} />
-        <Route path="/order" exact component={OrderPage} />
       </Switch>
     </Router>
   );
