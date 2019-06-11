446b6846fcead45cb6afd11de19fcdc8ac52ffe1
junjun955 <529665956@qq.com>
2019-06-11 20:06:05 +0800

    赵丽君提交router.js 这东西一直冲突

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import OrderLinePage from './routes/OrderLinePage'
 import CustomerPage from './routes/CustomerPage'
-<<<<<<< HEAD
-=======
 import OrderPage from './routes/OrderPage'
->>>>>>> 321f836eb162f8ced11f1b5319952c61df00a94e
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/orderLine" exact component={OrderLinePage} />
         <Route path="/customer" exact component={CustomerPage} />
+        <Route path="/order" exact component={OrderPage} />
       </Switch>
     </Router>
   );
