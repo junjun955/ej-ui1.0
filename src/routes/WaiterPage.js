178cefb0b150c06b379a3b0fd41469d9d9be5b1c
LiHY22 <9443766891@qq.com>
2019-06-12 14:06:28 +0800

    李昊洋增加了Waiter的部分代码

--- src/routes/WaiterPage.js
+++ src/routes/WaiterPage.js
@@ -4,6 +4,8 @@ import styles from './WaiterPage.css'
 // 导入组件
 import {Modal,Button, Table,message} from 'antd'
 import axios from '../utils/axios'
+import WaiterForm from './WaiterForm'
+
 
 // 组件类必须要继承React.Component，是一个模块，顾客管理子功能
 class WaiterPage extends React.Component {
@@ -13,7 +15,8 @@ class WaiterPage extends React.Component {
     this.state = {
       ids:[], // 批量删除的时候保存的id
       list:[],
-      loading:false
+      loading:false,
+      visible:false
     }
   }
   // 在生命周期钩子函数中调用重载数据
@@ -69,23 +72,62 @@ class WaiterPage extends React.Component {
       }
     });
   }
+  // 取消按钮的事件处理函数
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+  // 确认按钮的事件处理函数
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      // 表单校验完成后与后台通信进行保存
+      axios.post("/waiter/saveOrUpdate",values)
+      .then((result)=>{
+        message.success(result.statusText)
+        // 重置表单
+        form.resetFields();
+        // 关闭模态框
+        this.setState({ visible: false });
+        this.reloadData();
+      })
+      
+    });
+  };
+  // 将子组件的引用在父组件中进行保存，方便后期调用
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+  // 去添加
+  toAdd(){
+    this.setState({ visible:true})
+  }
+  // 去更新
+  toEdit(record){
+    alert(JSON.stringify(record));
+    // 将record值绑定表单中
+    this.setState({visible:true})
+  }
 
   // 组件类务必要重写的方法，表示页面渲染
   render(){
     // 变量定义
     let columns = [{
-      title:'ID',
-      dataIndex:'id'
-      },{
       title:'姓名',
       dataIndex:'realname'
     },{
       title:'手机号',
       dataIndex:'telephone'
     },{
-       title:'ID号',
-       dataIndex:'idcard'
-      },{
+      title:'ID号',
+      dataIndex:'idcard'
+    },{
+      title:'状态',
+      align:"center",
+      dataIndex:'status'
+    },{
       title:'操作',
       width:120,
       align:"center",
@@ -93,7 +135,7 @@ class WaiterPage extends React.Component {
         return (
           <div>
             <Button type='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
-            <Button type='link' size="small">修改</Button>
+            <Button type='link' size="small" onClick={this.toEdit.bind(this,record)}>修改</Button>
           </div>
         )
       }
@@ -116,7 +158,7 @@ class WaiterPage extends React.Component {
       <div className={styles.waiter}>
         <div className={styles.title}>工人管理</div>
         <div className={styles.btns}>
-          <Button>添加</Button> &nbsp;
+          <Button onClick={this.toAdd.bind(this)}>添加</Button> &nbsp;
           <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
           <Button type="link">导出</Button>
         </div>
@@ -129,6 +171,11 @@ class WaiterPage extends React.Component {
           columns={columns}
           dataSource={this.state.list}/>
 
+        <WaiterForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}/>
       </div>
     )
   }
