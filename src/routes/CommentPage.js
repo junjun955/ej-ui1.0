ad45cbfda4ab023cd12acbb5b4b12f9a47684c03
qpl123 <1695521701@qq.com>
2019-06-12 17:57:05 +0800

    Comment页面第六次修改

--- src/routes/CommentPage.js
+++ src/routes/CommentPage.js
@@ -4,6 +4,7 @@ import styles from './CommentPage.css'
 // 导入组件
 import {Modal,Button, Table,message} from 'antd'
 import axios from '../utils/axios'
+import CustomerForm from './CommentForm'
 
 // 组件类必须要继承React.Component，是一个模块，顾客管理子功能
 class CommentPage extends React.Component {
@@ -13,7 +14,8 @@ class CommentPage extends React.Component {
     this.state = {
       ids:[], // 批量删除的时候保存的id
       list:[],
-      loading:false
+      loading:false,
+      visible:false
     }
   }
   // 在生命周期钩子函数中调用重载数据
@@ -69,6 +71,45 @@ class CommentPage extends React.Component {
       }
     });
   }
+  // 取消按钮的事件处理函数
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+  // 确认按钮的事件处理函数
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      // 表单校验完成后与后台通信进行保存
+      axios.post("/comment/saveOrUpdate",values)
+      .then((result)=>{
+        message.success(result.statusText)
+        // 重置表单
+        form.resetFields();
+        // 关闭模态框
+        this.setState({ visible: false });
+        this.reloadData();
+      })
+      
+    });
+  };
+  // 将子组件的引用在父组件中进行保存，方便后期调用
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+  // 去添加
+  toAdd(){
+    this.setState({ visible:true})
+  }
+  // 去更新
+  toEdit(record){
+    alert(JSON.stringify(record));
+    // 将record值绑定表单中
+    this.setState({visible:true})
+  }
+
 
   // 组件类务必要重写的方法，表示页面渲染
   render(){
@@ -109,13 +150,13 @@ class CommentPage extends React.Component {
     };
     
     // 返回结果 jsx(js + xml)
-    return (
+     return (
       <div className={styles.comment}>
         <div className={styles.title}>顾客评论管理</div>
         <div className={styles.btns}>
-          <Button>添加</Button> &nbsp;
-          <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
-          <Button type="link">导出</Button>
+        <Button onClick={this.toAdd.bind(this)}>添加</Button> &nbsp;
+        <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
+        <Button type="link">导出</Button>
         </div>
         <Table 
           bordered
@@ -124,7 +165,14 @@ class CommentPage extends React.Component {
           loading={this.state.loading}
           rowSelection={rowSelection}
           columns={columns}
-          dataSource={this.state.list}/>
+          dataSource={this.state.list}
+             />
+          <CustomerForm
+            wrappedComponentRef={this.saveFormRef}
+            visible={this.state.visible}
+            onCancel={this.handleCancel}
+            onCreate={this.handleCreate}/>
+         
 
       </div>
     )
