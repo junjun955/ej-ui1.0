8225dfc1fb0a2cbc08eeb2fb1e164e3040e4cc97
lululu0 <1272741817@qq.com>
2019-06-20 18:39:51 +0800

    吕凯璐 js对象转化为字符串，切记先pull再push

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -9,9 +9,9 @@ axios.defaults.headers["Content-Type"]= "application/x-www-form-urlencoded";
 // 2. 拦截器配置
 axios.interceptors.request.use((config)=>{
   if(config.method === "post"){
-    // 将js对象转换为查询字符串
-    //config.data = qs.stringify(config.data,{ arrayFormat: 'repeat' })
-   config.data = qs.stringify(config.data,{ allowDots: true })
+    // 将js对象转换为查询字符串，用第一行是对的
+  config.data = qs.stringify(config.data,{ arrayFormat: 'repeat' })
+  // config.data = qs.stringify(config.data,{ allowDots: true })
   }
   return config;
 })
